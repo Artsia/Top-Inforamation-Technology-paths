6beb2dcfd3ad51e5806058ef8c18ab2d54bdd869
Artsia <65820314+Artsia@users.noreply.github.com>
2020-08-26 17:47:47 -0400

    initial function file for html file

---
+++ userData.js
@@ -0,0 +1,17 @@
+//when user enters a string and pushes enter button(event)
+//we want to do something(action) ie get the text entered and store it
+//on a sql relational db
+
+//we want to create a method/ function that does this action when user
+//enters string into form, where id = userName
+const button = document.querySelector('btn');
+
+function getTypedData()
+{
+    return Math.floor(Math.random() * (number+1));
+}
+
+btn.onclick = function() {
+    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    document.body.style.backgroundColor = rndCol;
+}
